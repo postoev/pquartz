9f4037df43ce9ac1f533e14b6edd275d80e43d0f
iOS <iosifmen@gmail.com>
2018-05-30 15:48:48 +0300

    pochemu on ran'she ne prosil eto sdelat'?
    pochemu on ran'she ne prosil eto sdelat'?
    
    pochemu on ran'she ne prosil eto sdelat'?

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect, session, url_for, request, g
+ from flask import render_template, flash, redirect, session, url_for, request, g
 from flask.ext.login import login_user, logout_user, current_user, login_required
 from werkzeug.urls import url_parse
 from . import application, db, bootstrap
